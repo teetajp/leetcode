6587693bd988ff0321a58bc9739d73cf7f4104b5
TJ Pavaritpong <teetajp@gmail.com>
2022-11-13 03:56:52 -0600

    Time: 59 ms (48.24%), Space: 13.9 MB (72.24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        for char in s:
+            if char in mapping:
+                if len(stack) == 0 or stack.pop() != mapping[char]:
+                    return False
+            else:
+                stack.append(char)
+        return len(stack) == 0
