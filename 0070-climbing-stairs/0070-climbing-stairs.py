232f58ac0c8a551f8b4aa0250014404553bea1fb
TJ Pavaritpong <teetajp@gmail.com>
2022-11-16 20:02:52 -0600

    Time: 55 ms (49.28%), Space: 13.9 MB (12.22%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        @lru_cache
+        def stepCounts(i):
+            if i == 1:
+                return 1
+            if i == 2:
+                return 2
+            return stepCounts(i-1) + stepCounts(i-2)
+        return stepCounts(n)
