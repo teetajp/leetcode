c758bdb2487a258661b5dd0de51d790afde50b71
TJ Pavaritpong <teetajp@gmail.com>
2022-11-16 21:08:19 -0600

    Time: 135 ms (95.59%), Space: 16.5 MB (59.72%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,18 +1,29 @@
 class Solution:
-    def calculateIslandArea(self, i, j):
-        if i < 0 or i >= len(self.grid) or j < 0 or j >= len(self.grid[0]) or self.grid[i][j] == 0:
-            return 0
-        self.grid[i][j] = 0
-        return 1 + self.calculateIslandArea(i+1, j) + self.calculateIslandArea(i-1, j) + self.calculateIslandArea(i, j+1) + self.calculateIslandArea(i, j-1)
         
-        
+    
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         # In each island cell, add 1 if grid is valid, otherwise add 0
         rows, cols, maxArea = len(grid), len(grid[0]), 0
-        self.grid = grid
+        
+        def calculateIslandArea(i: int, j: int) -> int:
+            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            return 1 + calculateIslandArea(i-1, j) + calculateIslandArea(i+1, j) + calculateIslandArea(i, j+1) + calculateIslandArea(i, j-1)
+        
         for i in range(rows):
             for j in range(cols):
-                maxArea = max(self.calculateIslandArea(i, j), maxArea)
+                if grid[i][j]:
+                    maxArea = max(calculateIslandArea(i, j), maxArea)
         
         return maxArea
 
+     # def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+     #    ans, n, m = 0, len(grid), len(grid[0])
+     #    def trav(i: int, j: int) -> int:
+     #        if i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == 0: return 0
+     #        grid[i][j] = 0
+     #        return 1 + trav(i-1, j) + trav(i, j-1) + trav(i+1, j) + trav(i, j+1)
+     #    for i, j in product(range(n), range(m)):
+     #        if grid[i][j]: ans = max(ans, trav(i, j))
+     #    return ans
