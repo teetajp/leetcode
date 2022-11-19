bc77d72dc44e6354af2bd38d7a4aeeea66bca3f6
TJ Pavaritpong <teetajp@gmail.com>
2022-11-18 19:13:11 -0600

    Time: 129 ms (43.74%), Space: 14.1 MB (20.51%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,8 +1,9 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         # Let minCost[i] be the cost of climbing the stairs from the ith step till the top of stairs
-        # minCost[i] = cost[i] + min(minCost[i+1], minCost[i+2])        if i < len(cost)
-        # 0                                                             if i >= len(cost)
+        # Our recursive formula is defined as:
+        #   minCost[i] = cost[i] + min(minCost[i+1], minCost[i+2])                     if i < len(cost)
+        #   minCost[i] = 0                                                             if i >= len(cost)
         minCost = [0 for i in range(len(cost) + 2)] # 2 placeholder value to represent when i >= len(cost) - 1
         
         for i in range(len(cost)-1, -1, -1):
