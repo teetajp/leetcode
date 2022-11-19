e045a18c92d52b8df23599eb5f70c90bbbfa4c22
TJ Pavaritpong <teetajp@gmail.com>
2022-11-18 21:05:14 -0600

    Time: 57 ms (57.75%), Space: 13.9 MB (19.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -11,14 +11,17 @@ class Solution:
         # We have to fill the array from with i <- n down to 0 to satisfy the dependencies
         # Also compute j=1 first before j=0
         
-        n = len(nums)
-        maxLoot = [[0 for i in range(n + 2)] for j in range(2)]
-        for i in range(n-1, -1, -1):
-            maxLoot[1][i] = nums[i] + maxLoot[0][i+2]
-            maxLoot[0][i] = max( maxLoot[1][i], maxLoot[1][i+1] )      
+#         n = len(nums)
+#         maxLoot = [[0 for i in range(n + 2)] for j in range(2)]
+#         for i in range(n-1, -1, -1):
+#             maxLoot[1][i] = nums[i] + maxLoot[0][i+2]
+#             maxLoot[0][i] = max( maxLoot[1][i], maxLoot[1][i+1] )      
                 
-        return maxLoot[0][0]
-        # O(n) time to fill the DP array, O(2n) space for DP memoization array
+#         return maxLoot[0][0]
+#         # O(n) time to fill the DP array, O(2n) space for DP memoization array
     
-            
+        maxLoot = [0, 0] # first index stores the max loot from the i-2th iteration, and second index stores current iteration's max loot
+        for loot in nums:
+            maxLoot[0], maxLoot[1] = maxLoot[1], max(maxLoot[1], loot + maxLoot[0])
+        return max(maxLoot)
             
