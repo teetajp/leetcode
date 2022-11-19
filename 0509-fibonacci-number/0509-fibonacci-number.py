0e549d02cb73b4b8c27fa8d9cb467de741c9cbaa
TJ Pavaritpong <teetajp@gmail.com>
2022-11-18 19:29:40 -0600

    Time: 57 ms (58.83%), Space: 13.8 MB (55.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     # @lru_cache(None)
     # def fib(self, n: int) -> int:
@@ -5,7 +6,12 @@ class Solution:
     #     return self.fib(n-1) + self.fib(n-2)
     
     def fib(self, n: int) -> int:
-        fib_memo = [0, 1]
+        # fib_memo = [0 for i in range(n+1)]
+        # for i in range(2, n+1):
+        #     fib_memo.append(fib_memo[i-2] + fib_memo[i-1])
+        if n == 0: return 0
+        fib_memo = deque([0, 1])
         for i in range(2, n+1):
-            fib_memo.append(fib_memo[i-2] + fib_memo[i-1])
-        return fib_memo[n]
+            fib_memo.append(fib_memo[0] + fib_memo[1])
+            fib_memo.popleft()
+        return fib_memo[1]
