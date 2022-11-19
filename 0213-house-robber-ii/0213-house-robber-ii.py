479f8118a2d04a6a328d5b86db434d1ae000af4b
TJ Pavaritpong <teetajp@gmail.com>
2022-11-18 22:02:07 -0600

    Time: 23 ms (99.78%), Space: 13.8 MB (72.44%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,25 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-
-        @lru_cache
-        def maxLoot(i: int, end: int, willRob: bool) -> int:
-            # end is exclusive
-            if i >= end:
-                return 0
-            if willRob:
-                return nums[i] + maxLoot(i+2, end, False)
-            return max( maxLoot(i, end, True), maxLoot(i+1, end, True) )
-        res = max( maxLoot(0, n-1, True), nums[n-1] + maxLoot(1, n-2, False))
-        if n >= 2:
-            res = max(res, maxLoot(1, n, True), nums[n-2] + maxLoot(0, n-3, False))
-        return res
-               
-        # Case 1: Rob 0th house, can't rob n-1th house, can't rob 1st house
-               # Check nums[2...n-1]
-        # Case 2: Rob n-1th house, can't rob 0th house, can't rob n-2th house
-               # Check nums[1...n-2]
-        # Case 3: Rob 1st house: can't rob 0th house or 2nd house
-               # Check nums[3...n]
-        # Case 4: Rob n-2th house, can't rob n-1th house, can't rob n-3th house
-               # Check nums[0...n-4]
+        def simple_rob(start: int, end: int):
+            maxLoot = [0, 0] # first index stores the max loot from the i-2th iteration, and second index stores current iteration's max loot
+            for i in range(start, end):
+                maxLoot[0], maxLoot[1] = maxLoot[1], max(maxLoot[1], nums[i] + maxLoot[0])
+            return maxLoot[1]
+        if len(nums) == 1: return nums[0]
+        return max(simple_rob(0, len(nums)-1), simple_rob(1, len(nums)))
