44a7fa8f752f5f3465bf144abdb57e226821af90
TJ Pavaritpong <teetajp@gmail.com>
2022-11-18 19:09:36 -0600

    Time: 67 ms (86.62%), Space: 14 MB (74.84%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -4,6 +4,7 @@ class Solution:
         # 0                                                             if i >= len(cost)
         n = len(cost)
         minCost = [0 for i in range(n + 2)] # 2 placeholder value to represent when i >= len(cost) - 1
+        minCost[n], minCost[n+1] = 0, 0
         
         for i in range(n-1, -1, -1):
             minCost[i] = cost[i] + min(minCost[i+1], minCost[i+2])
