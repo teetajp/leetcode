2c2381fcbf0481d9107b67913d2ff0a7c6bd12f6
TJ Pavaritpong <teetajp@gmail.com>
2022-11-18 19:24:41 -0600

    Time: 28 ms (97.96%), Space: 13.9 MB (9.47%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,11 @@
 class Solution:
-    @lru_cache(None)
+    # @lru_cache(None)
+    # def fib(self, n: int) -> int:
+    #     if n <= 1: return n
+    #     return self.fib(n-1) + self.fib(n-2)
+    
     def fib(self, n: int) -> int:
-        if n <= 1: return n
-        return self.fib(n-1) + self.fib(n-2)
+        fib_memo = [0, 1]
+        for i in range(2, n+1):
+            fib_memo.append(fib_memo[i-2] + fib_memo[i-1])
+        return fib_memo[n]
