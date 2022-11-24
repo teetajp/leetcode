42c0dca6e28b9c8243af908a9a1d3326896ef366
TJ Pavaritpong <teetajp@gmail.com>
2022-11-23 20:18:29 -0600

    Time: 88 ms (55.05%), Space: 14.4 MB (44.55%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,8 +5,9 @@ class Solution:
         # If less than, then check val in first col of row, if less than, recurse to lower row
         # If more than, then check val in last col of row, if target is >, then recurse to upper row
         rows, cols = len(matrix), len(matrix[0])
+        l, r = 0, rows * cols - 1
         
-        def binarySearch(l: int, r: int) -> bool:
+        while l <= r:
             if r < l:
                 return False
             mid_idx = l + (r - l) // 2 # median, rounded down
@@ -15,10 +16,36 @@ class Solution:
             if mid_val == target:
                 return True
             elif target < mid_val:
-                return binarySearch(l, mid_idx - 1)
+                r = mid_idx - 1
             elif target > mid_val:
-                return binarySearch(mid_idx + 1, r)
+                l = mid_idx + 1
+            
         
-        return binarySearch(0, rows * cols - 1)
+        return False
     # O(log m*n) time for binary search
-    # O(log m*n) space for recursion stack
+    # O(1) space
+    
+#     def searchMatrixRecursive(self, matrix: List[List[int]], target: int) -> bool:
+#         # Start at middle value of middle row
+#         # If middle value is target val, return
+#         # If less than, then check val in first col of row, if less than, recurse to lower row
+#         # If more than, then check val in last col of row, if target is >, then recurse to upper row
+#         rows, cols = len(matrix), len(matrix[0])
+        
+#         def binarySearch(l: int, r: int) -> bool:
+#             if r < l:
+#                 return False
+#             mid_idx = l + (r - l) // 2 # median, rounded down
+#             mid_val = matrix[mid_idx // cols][mid_idx % cols] # index by row-major order
+            
+#             if mid_val == target:
+#                 return True
+#             elif target < mid_val:
+#                 return binarySearch(l, mid_idx - 1)
+#             elif target > mid_val:
+#                 return binarySearch(mid_idx + 1, r)
+            
+        
+#         return binarySearch(0, rows * cols - 1)
+#     # O(log m*n) time for binary search
+#     # O(log m*n) space for recursion stack
