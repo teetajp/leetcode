fef199501ad20db5adaf0b7b802d97e5e7d78a3d
TJ Pavaritpong <teetajp@gmail.com>
2022-11-23 18:53:23 -0600

    Time: 468 ms (93.66%), Space: 25.9 MB (89.97%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # store each value in a hashmap and put False as default
+        # set to true if we encounter the number
+        # if we see it again and the value is in the hashmap (as True), we return False
+        # else return True
+        # O(n) time complexity since we iterate through the whole array
+        # O(1) space since the values are constrained by the range of ints
+        #   - maybe O(n) space? not sure, since hash map since grows if there are more distinct vals
+        
+        values_seen = dict()
+        
+        for val in nums:
+            if val in values_seen:
+                return True
+            values_seen[val] = True
+        
+        return False
