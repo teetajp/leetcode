0603aa503963677c3750bdca5f5429ea5cceca4c
TJ Pavaritpong <teetajp@gmail.com>
2022-11-24 02:57:13 -0600

    Time: 24 ms (56.05%), Space: 18.5 MB (38.00%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,12 +3,10 @@ public:
     int removeDuplicates(vector<int>& nums) {
         // Perform "Bubble Sort" with each distinct element bubbling to the front
         int l = 1; // left pointer to keep track of place to insert
-        int r = 1; // right pointer which checks for the next distinct element to move to front
-        while (r < nums.size()) {
+        for (int r = 1; r < nums.size(); r++) {
             if (nums[r] != nums[r-1]) {
                 nums[l++] = nums[r];
             }
-            r++;
         }
         return l;
     }
