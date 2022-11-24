e6f0046c94081eda9ffbec9ebd5fd5fe7e6750de
TJ Pavaritpong <teetajp@gmail.com>
2022-11-23 19:27:02 -0600

    Time: 27 ms (66.98%), Space: 10.9 MB (31.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,11 +3,10 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> num_idx; // stores index of numbers, must be careful since default is 0 when undefined
         for (int i = 0; i < nums.size(); i++) {
-            unordered_map<int, int>::iterator diff_idx = num_idx.find(target - nums[i]);
             
-            if (diff_idx != num_idx.end()) {
-                return vector<int>{i, diff_idx->second};
-            }
+            if (num_idx.find(target - nums[i]) != num_idx.end())
+                return vector<int>{i, num_idx[target - nums[i]]};
+            
             num_idx[nums[i]] = i;
         }
         return vector<int>{-1, -1}; // placeholder (should not reach here)
