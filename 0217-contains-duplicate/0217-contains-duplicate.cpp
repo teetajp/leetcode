80eaa362ee6dcd3bf6b6a7c0f296c87e3c81da5b
TJ Pavaritpong <teetajp@gmail.com>
2022-11-23 19:05:13 -0600

    Time: 163 ms (69.18%), Space: 51.3 MB (70.84%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, bool> values_seen;
+        for (auto val : nums) {
+            if (values_seen[val] == true) {
+                return true;
+            }
+            values_seen[val] = true;
+        }
+        return false;
+    }
+};
