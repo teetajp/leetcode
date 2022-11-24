80cffc19d0d24d18feff9c0418af29d4748a4f17
TJ Pavaritpong <teetajp@gmail.com>
2022-11-23 19:59:37 -0600

    Time: 101 ms (20.74%), Space: 14.5 MB (44.55%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -9,18 +9,18 @@ class Solution:
         def binarySearch(l: int, r: int) -> bool:
             if r < l:
                 return False
-            mid = l + (r - l) // 2 # median, rounded down
-            mid_idx = ( mid // cols, (mid % cols) )# need to get index by row-major order
+            mid_idx = l + (r - l) // 2 # median, rounded down
+            mid_val = matrix[mid_idx // cols][mid_idx % cols] # index by row-major order
             
-            if (matrix[mid_idx[0]][mid_idx[1]] == target or
+            if (mid_val == target or
                 matrix[l // cols][l % cols] == target or
                 matrix[r // cols][r % cols] == target
                ):
                 return True
-            elif target < matrix[mid_idx[0]][mid_idx[1]]:
-                return binarySearch(l+1, mid - 1)
-            elif target > matrix[mid_idx[0]][mid_idx[1]]:
-                return binarySearch(mid + 1, r - 1)
+            elif target < mid_val:
+                return binarySearch(l+1, mid_idx - 1)
+            elif target > mid_val:
+                return binarySearch(mid_idx + 1, r - 1)
         
         return binarySearch( 0, rows * cols - 1)
     # O(log n) time for binary search
