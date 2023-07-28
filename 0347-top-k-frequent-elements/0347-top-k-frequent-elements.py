095663caecf920dc938dc31c5166ae9f05d09ee7
TJ Pavaritpong <teetajp@gmail.com>
2023-07-28 10:28:13 -0500

    Time: 96 ms (99.64%), Space: 21 MB (91.23%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,27 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # initialize a min heap
+        # iterate over the nums array O(n)
+        
+        # --- add/increment count of each int iterated from the nums array onto the min heap (max size k) - might be problematic? ---
+        # OR use hashmap to keep a count of each freq, then iterate over the entries while using a min heap of size k
+        
+        # Time Complexity: O(n) to iterate and count freq, O(k) + O((n-k)*logk) worst case for minheap
+        # Space Complexity: O(k) for hashmap, O(k) for minheap => total O(k) if excluding input array
+        freq = {}
+        for n in nums:
+            freq[n] = freq[n] + 1 if n in freq else 1
+        
+        minHeap = []
+        for idx, n in enumerate(freq):
+            if idx < k:
+                minHeap.append((freq[n], n))
+                if len(minHeap) == k:
+                    heapq.heapify(minHeap)
+            else:
+                if freq[n] > minHeap[0][0]:
+                    heapq.heappushpop(minHeap, (freq[n], n))
+        
+        return [key for count, key in minHeap] 
