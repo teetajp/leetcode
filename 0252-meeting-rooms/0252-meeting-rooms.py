59de76e36d9e0215aaba52bc3ec7a05b326c88de
TJ Pavaritpong <teetajp@gmail.com>
2023-09-24 13:41:45 -0500

    Time: 83 ms (35.11%), Space: 19.4 MB (99.93%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,7 +1,7 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         # check whether all meetings are disjoint
-        intervals.sort(key=lambda x: (x[0], x[1]))
+        intervals.sort()
         
         for i, intv in enumerate(intervals[:-1]):
             if intv[1] > intervals[i+1][0]:
