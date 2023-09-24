b8aac004b749c48d18cb3b4a5e6729c56b985b7f
TJ Pavaritpong <teetajp@gmail.com>
2023-09-24 13:23:59 -0500

    Time: 42 ms (46.72%), Space: 16.2 MB (86.57%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,6 +2,7 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         # need carry bits
         # look for number that does not end in 9 - set previous digits to 0
+        
         for i in range(len(digits)-1, -1, -1):
             if digits[i] == 9:
                 digits[i] = 0
@@ -9,5 +10,4 @@ class Solution:
                 digits[i] += 1
                 # need to carry
                 return digits
-        digits.insert(0, 1)
-        return digits
+        return [1] + digits
