954bbe4ed38069329b0f8f0c15429d59e9ba572d
TJ Pavaritpong <teetajp@gmail.com>
2023-09-24 13:40:49 -0500

    Time: 86 ms (21.00%), Space: 19.6 MB (87.72%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -3,8 +3,8 @@ class Solution:
         # check whether all meetings are disjoint
         intervals.sort(key=lambda x: (x[0], x[1]))
         
-        for i in range(len(intervals)-1):
-            if intervals[i][1] > intervals[i+1][0]:
+        for i, intv in enumerate(intervals[:-1]):
+            if intv[1] > intervals[i+1][0]:
                 return False
         
         return True
