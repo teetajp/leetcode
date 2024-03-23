753d5cdeb9ee22dd33a77f0ae8a0ab2ecfbfd064
TJ Pavaritpong <teetajp@gmail.com>
2024-03-23 17:50:40 -0500

    Time: 847 ms (98.23%), Space: 31.4 MB (37.08%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,6 +1,6 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = []
+        answer = [0] * len(temperatures)
         stack = [] # keeps track of day indices that have not seen warmer day
         for i in range(len(temperatures)):            
             while stack and temperatures[stack[-1]] < temperatures[i]:
@@ -8,7 +8,7 @@ class Solution:
                 prev_idx = stack.pop() # get most recent unupdated cooler day
                 answer[prev_idx] = i - prev_idx # num of days between
                           
-            answer.append(0)
+            #answer.append(0)
             stack.append(i)
         
         return answer
