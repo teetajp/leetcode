31c6095999aecb66fa7c326d74a37e1a51a4fa43
TJ Pavaritpong <teetajp@gmail.com>
2024-03-23 17:46:12 -0500

    Time: 856 ms (95.49%), Space: 31.7 MB (24.99%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,18 +1,27 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        answer = [0] * n
-        # use stack to keep track of days that still need warmer temp
-        # the stack will only increase in size as long as the item on top is less than the item below it (otherwise it would be popped as the top element, which is the newest day, would be warmer than the previous in that case)
-        stack = [(0, temperatures[0])]
-        
-        for i in range(1, n):
-            while stack and temperatures[i] > stack[-1][1]:
-                answer[stack[-1][0]] = i - stack[-1][0]
-                stack.pop()
-                
-            stack.append((i, temperatures[i]))
+        # if we go forward, we have to keep track of the indices to update
+        # temp[i-2]=74 temp[i-1] = 75, temp[i] = 73, temp[i+1] = 72, temp[i+2] = 74
+        # then, we update indices i and i+1
+        # need to keep order consistent
+
+        # keep a stack on indices we have yet to seen warmer temp
+        # forward pass
+        # when we see a new temp that is higher,
+        # > pop and update everything from top of stack till not higher
+        answer = []
+        stack = [] # keeps track of day indices
+        for i in range(len(temperatures)):
+            cur_tmp = temperatures[i]
             
+            while stack and temperatures[stack[-1]] < cur_tmp:
+                # while exists a previous day cooler than ith day that has not been updated
+                prev_idx = stack.pop() # get most recent unupdated cooler day
+                answer[prev_idx] = i - prev_idx # num of days between
+                
+                
+            answer.append(0)
+            stack.append(i)
+        
+        
         return answer
-    # Time Complexity: O(n) -> for loop iterates over n elems, inner loop max n
-    # Space Complexity: O(n) -> for stack and answer
