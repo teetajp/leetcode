5a5a6115871b083955b4cea4cc92fcae763084d4
TJ Pavaritpong <teetajp@gmail.com>
2024-03-23 17:49:04 -0500

    Time: 877 ms (79.37%), Space: 31.7 MB (24.99%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,25 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # if we go forward, we have to keep track of the indices to update
-        # temp[i-2]=74 temp[i-1] = 75, temp[i] = 73, temp[i+1] = 72, temp[i+2] = 74
-        # then, we update indices i and i+1
-        # need to keep order consistent
-
-        # keep a stack on indices we have yet to seen warmer temp
-        # forward pass
-        # when we see a new temp that is higher,
-        # > pop and update everything from top of stack till not higher
         answer = []
-        stack = [] # keeps track of day indices
+        stack = [] # keeps track of day indices that have not seen warmer day
         for i in range(len(temperatures)):            
             while stack and temperatures[stack[-1]] < temperatures[i]:
                 # while exists a previous day cooler than ith day that has not been updated
                 prev_idx = stack.pop() # get most recent unupdated cooler day
                 answer[prev_idx] = i - prev_idx # num of days between
-                
-                
+                          
             answer.append(0)
             stack.append(i)
         
-        
         return answer
+    # time: O(n) for accessing each element at most twice
+    # space: O(n) for stack and answer list
