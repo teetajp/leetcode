072b27a2e429442b0b38d1831b1bf226b6cd30cc
TJ Pavaritpong <teetajp@gmail.com>
2024-03-26 17:18:59 -0500

    Time: 79 ms (91.61%), Space: 17.4 MB (38.43%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,53 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        m, n = len(s), len(t)
+        freqs = {}
+        
+        for char in t:
+            freqs[char] = freqs.get(char, 0) + 1
+            
+        cur_freq_met, min_freq_met = 0, len(freqs)
+            
+        # iterating through s once
+        # keep a counter of character freqs that have been met (freq of 0 or lower)
+        # if character not in freqs, then ignore it
+        # can use queue or two pointers for the window
+        # => first grow window on right until hit min (increment char freqs), then shrink (decrement char freqs) from left to minimum
+        l, r = 0, 0
+        min_window_idx = None
+        while r < m:
+            # expand window to include at least the freq of each char in t
+            right_char = s[r]
+            
+            if right_char in freqs:
+                freqs[right_char] -= 1
+                
+                if freqs[right_char] == 0:
+                    cur_freq_met += 1
+            
+            while cur_freq_met == min_freq_met:
+                # shrink window from left to get min window for this instance
+                cur_window_len = r - l + 1
+                
+                if min_window_idx is None or (min_window_idx[1] - min_window_idx[0] + 1 > cur_window_len):
+                    if cur_window_len == n: # found best possible window: return early
+                        return s[l : r + 1]
+                    min_window_idx = (l, r) # update current min window
+                
+                # shrink left side of window
+                left_char = s[l]
+                
+                if left_char in freqs:
+                    # check if shrinking window makes the char freq lower than in t
+                    if freqs[left_char] == 0:
+                        cur_freq_met -= 1
+                        
+                    freqs[left_char] += 1
+                l += 1 # move left pointer
+                    
+            r += 1
+            
+            
+            
+            
+        return s[min_window_idx[0] : min_window_idx[1] + 1] if min_window_idx else ""
