8d4bfc90c5969ef59d8571494c7e6e65fe23dbbb
TJ Pavaritpong <teetajp@gmail.com>
2024-04-01 15:18:44 -0500

    Time: 647 ms (89.32%), Space: 33.9 MB (34.54%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -6,16 +6,14 @@ class Solution:
         
         for i in range(n):
             cur_height = heights[i]
-            best_cur_rect = [cur_height, i]
+            start_idx = i
             
             while stack and stack[-1][0] > cur_height:
-                prev_rect = stack.pop()
-                best_cur_rect[1] = min(best_cur_rect[1], prev_rect[1])
+                prev_height, prev_idx = stack.pop()
+                max_area = max(max_area, prev_height * (i - prev_idx))
+                start_idx = prev_idx
                 
-                max_area = max(max_area, prev_rect[0] * (i - prev_rect[1]))
-            max_area = max(max_area, best_cur_rect[0] * (i - best_cur_rect[1] + 1))
-                
-            stack.append(best_cur_rect)
+            stack.append((cur_height, start_idx))
                 
         for height, start_idx in stack:
             max_area = max(max_area, height * (n - start_idx))
