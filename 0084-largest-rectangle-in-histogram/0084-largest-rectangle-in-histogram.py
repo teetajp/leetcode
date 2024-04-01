a755f3191c1a4a31b01be31c0e716ee5363d7b78
TJ Pavaritpong <teetajp@gmail.com>
2024-04-01 15:14:56 -0500

    Time: 803 ms (8.69%), Space: 35.4 MB (9.05%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,35 +1,14 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        # current area, compared to min(cur_height, next_height) * (cur_len + 1)
-        # vs. cur_height * 1
-        
-        # tuples of (rect_height, rect_start)
-        # area is calculated on the fly since we need to update it often otherwise
-        stack = [[heights[0], 0]]
-        max_area = heights[0]
-        
+        stack = []
+        max_area = 0
         n = len(heights)
-        for i in range(1, n):
-            # if we continue the rectangle from previous,
-            # then the area would have a bounded height of previous height
-            # need to compare starting from current
-            # and concat from previous
-            
-            # should prefer lower height as it allows us to continue more
-            # if cur rect has height lower than prev in stack
-            # => need to bound everything by the lower height,
-            # => so, pop elems that have higher height and update them
-            
-            # if cur rect has higher height, can continue by prev rect with same height but increasing width by 1
-            # so keep tallest rect on top of stack to easily compare/remove
-            
-            # monotonically increasing stack
+        
+        for i in range(n):
             cur_height = heights[i]
             best_cur_rect = [cur_height, i]
             
-            while stack and (stack[-1][0] > cur_height):
-                # pop the top of the stack until the rect on top has height smaller or equal to
-                # current bar height, calculating the area and comparing against the max area rect
+            while stack and stack[-1][0] > cur_height:
                 prev_rect = stack.pop()
                 best_cur_rect[1] = min(best_cur_rect[1], prev_rect[1])
                 
