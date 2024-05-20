c5f504c89f2058f3ee89cfe026a86d069b5009a5
TJ Pavaritpong <teetajp@gmail.com>
2024-05-20 16:48:02 -0500

    Time: 135 ms (35.73%), Space: 31.5 MB (27.51%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -12,41 +12,37 @@ class Trie:
         
 
     def insert(self, word: str) -> None:
-        self.word = word
-        node = self.findNodeCreate(word)
-        node.isWordSuffix = True
-
-    def search(self, word: str) -> bool:
-        self.word  = word
-        node = self.findNodeStop(self.root, 0)
-        return node and node.isWordSuffix
-        
-
-    def startsWith(self, prefix: str) -> bool:
-        self.word = prefix
-        node = self.findNodeStop(self.root, 0)
-        # check if current node is a word or prefix of some word
-        return node and (node.isWordSuffix or len(node.children) > 0)
-            
-    
-    def findNodeCreate(self, word: int) -> Node:
         # create nodes while traversing path
         idx = 0
         cur = self.root
         while idx < len(word):
             cur = cur.children[word[idx]]
             idx += 1
-        return cur
+
+        cur.isWordSuffix = True
+
+    def search(self, word: str) -> bool:
+        node = self.findNodeStop(word)
+        return node and node.isWordSuffix
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.findNodeStop(prefix)
+        # check if current node is a word or prefix of some word
+        return node and (node.isWordSuffix or len(node.children) > 0)
     
-    def findNodeStop(self, node: Node, idx: int) -> Node:
+    def findNodeStop(self, word: str) -> Node:
         # search for word, stopping early
         #   if node we're in doesn't have suffix we are looking for
-        if idx < len(self.word) and self.word[idx] in node.children:
-            return self.findNodeStop(node.children[self.word[idx]], idx+1)
-        elif idx == len(self.word):
-            return node
-        else:
-            return None
+        idx = 0
+        cur = self.root
+        while idx < len(word):
+            if word[idx] in cur.children:
+                cur = cur.children[word[idx]]
+                idx += 1
+            else:
+                return None
+        return cur
 
 
 # Your Trie object will be instantiated and called as such:
