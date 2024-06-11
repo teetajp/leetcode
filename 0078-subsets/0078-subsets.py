3b56afd0803c684532a45399b3686605d0e5f18f
TJ Pavaritpong <teetajp@gmail.com>
2024-06-10 20:41:28 -0500

    Time: 29 ms (94.27%), Space: 16.6 MB (73.66%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        results = []
-        results.append([]) # empty set
+        # no duplicate subsets possible as long as there are one of
+        # each unique element in the array and we change each iteration
+        res = [[]] # start with set containing empty set
         
-        for n in nums:
-            # make a copy of all elements in the results list and add current elem to it
-            # then append all of this to the results list
-            prev_len = len(results)
-            for i in range(prev_len):
-                results.append(results[i] + [n])
+        for int_elem in nums:
+            for i in range(len(res)):
+                res.append(res[i].copy() + [int_elem])
+                
         
-        return results
+        
+        return res
