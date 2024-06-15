4815454e72517942634f29edb16886c174f4454a
TJ Pavaritpong <teetajp@gmail.com>
2024-06-15 16:37:51 -0500

    Time: 57 ms (37.69%), Space: 16.6 MB (34.72%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort() # sort to ensure candidates are ordered
+        
+        # try in decreasing or increasing order of candidate magnitude
+        res = []
+        
+        def combinationSumRec(combo: List[int], cand_idx: int, target: int):
+            
+            if target > 0 and cand_idx < len(candidates):
+                # can still add more numbers
+                
+                # option 1: try same candidate
+                combo.append(candidates[cand_idx])
+                combinationSumRec(combo, cand_idx, target - candidates[cand_idx])
+                del combo[-1]
+                
+                combinationSumRec(combo, cand_idx + 1, target)
+                
+                
+            if target == 0:
+                # our combo sums up to target
+                res.append(combo.copy())         
+                
+        combinationSumRec([], 0, target)
+                
+        return res
