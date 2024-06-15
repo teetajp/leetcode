3fc0ab2bdd0de2178d7854e30d516ce393c529f4
TJ Pavaritpong <teetajp@gmail.com>
2024-06-15 17:14:45 -0500

    Time: 31 ms (99.75%), Space: 16.5 MB (66.22%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort(reverse=True)
+        
+        res = []
+        
+        def comboSumRec(combo: List[int], cand_idx: int, target: int):
+            if target == 0:
+                res.append(combo.copy())
+                
+            elif target > 0 and cand_idx < len(candidates):
+                # option 1: include this num
+                if candidates[cand_idx] <= target:
+                    combo.append(candidates[cand_idx])
+                    comboSumRec(combo, cand_idx + 1, target - candidates[cand_idx])
+                    combo.pop()
+                    
+                # option 2: if skip this num, then must skip that number completely
+                while ( cand_idx + 1 < len(candidates) and
+                        candidates[cand_idx] == candidates[cand_idx + 1] ):
+                    cand_idx += 1 # increment until we see another unique candidate
+                
+                if cand_idx + 1 < len(candidates):
+                    comboSumRec(combo, cand_idx + 1, target)
+        
+        
+        comboSumRec([], 0, target)
+        return res
