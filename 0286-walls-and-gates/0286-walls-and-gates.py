4b0035ce6aa69a5c65b4eafded06ab19328b6923
TJ Pavaritpong <teetajp@gmail.com>
2024-06-15 18:29:42 -0500

    Time: 181 ms (68.50%), Space: 19.6 MB (53.32%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -12,29 +12,29 @@ class Solution:
         m, n = len(rooms), len(rooms[0])
         queue = deque() # yet to explore
         
+        def addEmptyRoom(i, j, dist):
+            if i >= 0 and i < m and j >= 0 and j < n and rooms[i][j] == INF:
+                queue.append( (i, j, dist) )
+                
         # find the gates and add them to the queue to explore outwards
         for i in range(m):
             for j in range(n):
                 # cell is a gate
                 if rooms[i][j] == 0:
                     # add cells to explore
-                    queue.append( (i-1, j, 1) )
-                    queue.append( (i, j-1, 1) )
-                    queue.append( (i+1, j, 1) )
-                    queue.append( (i, j+1, 1) )
+                    addEmptyRoom(i-1, j, 1)
+                    addEmptyRoom(i, j-1, 1)
+                    addEmptyRoom(i+1, j, 1)
+                    addEmptyRoom(i, j+1, 1)
         
-        def isEmptyRoom(i, j):
-            return i >= 0 and i < m and j >= 0 and j < n and rooms[i][j] == INF
-        
-        # if a room has been visited earlier than another, it is guaranteed to have
-        # a lower distance due to BFS
+        # if a room has been visited earlier than another,
+        # then it is guaranteed to have lower distance due to BFS
         while queue:
             i, j, dist = queue.popleft()
-            if not isEmptyRoom(i, j):
-                continue # room out of bound or visited with shorter dist or not empty
             
-            rooms[i][j] = dist
-            queue.append( (i-1, j, dist + 1) )
-            queue.append( (i, j-1, dist + 1) )
-            queue.append( (i+1, j, dist + 1) )
-            queue.append( (i, j+1, dist + 1) )
+            if rooms[i][j] == INF:
+                rooms[i][j] = dist
+                addEmptyRoom(i-1, j, dist + 1)
+                addEmptyRoom(i, j-1, dist + 1)
+                addEmptyRoom(i+1, j, dist + 1)
+                addEmptyRoom(i, j+1, dist + 1)
