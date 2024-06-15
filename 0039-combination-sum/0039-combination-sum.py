ca760825f6359d83f5970df90d6631634d1c88c9
TJ Pavaritpong <teetajp@gmail.com>
2024-06-15 16:41:34 -0500

    Time: 35 ms (99.45%), Space: 16.5 MB (73.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort(reverse=True)
         res = []
         
         def combinationSumRec(combo: List[int], cand_idx: int, target: int):
@@ -7,10 +8,11 @@ class Solution:
             if target > 0 and cand_idx < len(candidates):
                 # can still add more numbers
                 
-                # option 1: try same candidate
-                combo.append(candidates[cand_idx])
-                combinationSumRec(combo, cand_idx, target - candidates[cand_idx])
-                del combo[-1]
+                # option 1: try same candidate if not too big
+                if candidates[cand_idx] <= target:
+                    combo.append(candidates[cand_idx])
+                    combinationSumRec(combo, cand_idx, target - candidates[cand_idx])
+                    del combo[-1]
                 
                 # option 2: try another candidate
                 combinationSumRec(combo, cand_idx + 1, target)
