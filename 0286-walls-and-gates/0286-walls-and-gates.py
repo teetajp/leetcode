6300823feeac89c84103c6e649844f22803a621d
TJ Pavaritpong <teetajp@gmail.com>
2024-06-15 18:37:02 -0500

    Time: 179 ms (71.69%), Space: 22.2 MB (37.63%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -12,31 +12,30 @@ class Solution:
         m, n = len(rooms), len(rooms[0])
         queue = deque() # yet to explore
         
-        def addEmptyRoom(i, j, dist):
-            if i >= 0 and i < m and j >= 0 and j < n:
-                queue.append( (i, j, dist) )
-                
         # find the gates and add them to the queue to explore outwards
         for i in range(m):
             for j in range(n):
                 # cell is a gate
                 if rooms[i][j] == 0:
                     # add cells to explore
-                    addEmptyRoom(i-1, j, 1)
-                    addEmptyRoom(i, j-1, 1)
-                    addEmptyRoom(i+1, j, 1)
-                    addEmptyRoom(i, j+1, 1)
+                    queue.append( (i-1, j, 1) )
+                    queue.append( (i, j-1, 1) )
+                    queue.append( (i+1, j, 1) )
+                    queue.append( (i, j+1, 1) )
         
-        # if a room has been visited earlier than another,
-        # then it is guaranteed to have lower distance due to BFS
+        def isEmptyRoom(i, j):
+            return i >= 0 and i < m and j >= 0 and j < n and rooms[i][j] == INF
+        
+        # if a room has been visited earlier than another, it is guaranteed to have
+        # a lower distance due to BFS
         while queue:
             i, j, dist = queue.popleft()
+            if not isEmptyRoom(i, j):
+                continue # room out of bound or visited with shorter dist or not empty
             
-            if rooms[i][j] == INF:
-                # ensure room has not yet been visited
-                rooms[i][j] = dist
-                # add neighbors
-                addEmptyRoom(i-1, j, dist + 1)
-                addEmptyRoom(i, j-1, dist + 1)
-                addEmptyRoom(i+1, j, dist + 1)
-                addEmptyRoom(i, j+1, dist + 1)
+            
+            rooms[i][j] = dist
+            queue.append( (i-1, j, dist + 1) )
+            queue.append( (i, j-1, dist + 1) )
+            queue.append( (i+1, j, dist + 1) )
+            queue.append( (i, j+1, dist + 1) )
