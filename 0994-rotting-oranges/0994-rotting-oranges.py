4b75234f1114838f0d5d3283dff1e1f29996a73c
TJ Pavaritpong <teetajp@gmail.com>
2024-06-15 19:39:49 -0500

    Time: 49 ms (65.06%), Space: 16.4 MB (87.13%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,42 +5,35 @@ class Solution:
         # BFS with each "level" as minute
         m, n = len(grid), len(grid[0])
         rotting_q = deque()
-        fresh_set = set()
-        visited = [[False for col in range(n)] for row in range(m)]
-        DIRS = ((-1, 0), (0, -1), (1, 0), (0, 1))
+        fresh_count = 0
         # check that all oranges has a path to at least one rotten orange (?)
         
         for i in range(m):
             for j in range(n):
                 # identify fresh and rotten oranges
                 if grid[i][j] == 1:
-                    fresh_set.add( (i, j) )
+                    fresh_count += 1
                 elif grid[i][j] == 2:
                     # add oranges
                     rotting_q.append( (i, j) )
-                    visited[i][j] = True
-
-                    
-        def addRottingOrange(i, j):
-            # adds fresh orange to the rotting queue if its in bounds
-            if 0 <= i < m and 0 <= j < n and grid[i][j] == 1 and not visited[i][j]:
-                rotting_q.append( (i, j) )
-                visited[i][j] = True
-                grid[i][j] = 2 # set to rotten
-                fresh_set.discard( (i, j) )
             
         elapsed = 0
+        DIRS = ((-1, 0), (0, -1), (1, 0), (0, 1))
         
-        while rotting_q and fresh_set:
+        while rotting_q and fresh_count:
             # iterate over each orange that just rot this minute
             for i in range(len(rotting_q)):
                 cur_x, cur_y = rotting_q.popleft()
                 
                 for dx, dy in DIRS:
                     new_x, new_y = cur_x + dx, cur_y + dy
-                    addRottingOrange(new_x, new_y)
+                    # adds fresh orange to the rotting queue if its in bounds
+                    if 0 <= new_x < m and 0 <= new_y < n and grid[new_x][new_y] == 1:
+                        rotting_q.append( (new_x, new_y) )
+                        grid[new_x][new_y] = 2 # set to rotten
+                        fresh_count -= 1
                 
             elapsed += 1
             
         
-        return elapsed if not fresh_set else -1
+        return elapsed if fresh_count == 0 else -1
