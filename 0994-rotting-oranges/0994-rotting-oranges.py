8022e832f79a532f146d6eb794f9ab70333b6ae7
TJ Pavaritpong <teetajp@gmail.com>
2024-06-15 19:25:24 -0500

    Time: 52 ms (46.05%), Space: 16.4 MB (98.44%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -39,11 +39,6 @@ class Solution:
                 for dx, dy in DIRS:
                     new_x, new_y = cur_x + dx, cur_y + dy
                     addRottingOrange(new_x, new_y)
-                    
-                    # break early
-                    if not fresh_set:
-                        return elapsed + 1 if not fresh_set else -1
-                    
                 
             elapsed += 1
             
