5e21cce9cccb66d92eaee817955a5d84805824c4
TJ Pavaritpong <teetajp@gmail.com>
2024-06-22 16:41:48 -0500

    Time: 80 ms (94.97%), Space: 18.1 MB (90.28%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,34 +1,35 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        """Run DFS on every node since there could be more than 1 connected component.
-        - Need a way to keep track of number of connected components and visited nodes.
-        - Some nodes may not have edges and are considered a separate connected component
-        XXX: How to check for distinct CC?
-        XXX: Need to watch out for cycles
-        - can do union find or DFS
-        """
+        """Union-find algorithm to count number of disjoint sets / connected components"""
+        parent = [i for i in range(n)]
+        rank = [1] * n
         
-        parent = list(range(n))
-
-        def find(x):
-            while parent[x] != x:
-                x = parent[x]
-            return x
-
-        def union(x, y):
-            rootX = find(x)
-            rootY = find(y)
-            if rootX != rootY:
-                parent[rootY] = rootX
-
+        def find(node):
+            """Find the parent of the set"""
+            while node != parent[node]:
+                parent[node] = parent[parent[node]] # compress path; XXX: what about rank?
+                node = parent[node]
+            return node
+            
+        def union(node1, node2):
+            """Merge two sets together if possible"""
+            parent1, parent2 = find(node1), find(node2)
+            
+            if parent1 == parent2:
+                return # same parent, do nothing
+            
+            # union by rank
+            if rank[parent1] >= rank[parent2]:
+                rank[parent1] += rank[parent2]
+                parent[parent2] = parent1
+            else:
+                rank[parent2] += rank[parent1]
+                parent[parent1] = parent2
+            
+            
+        
         for a, b in edges:
             union(a, b)
-
-        # Count the number of unique roots
-        numComponents = len(set(find(i) for i in range(n)))
         
-        return numComponents
-
-    
-    # runtime: O(E + V)
-    # space: O(E + V)
+        # count the number of connected components by checking for nodes with no outgoing edge
+        return sum(parent[i] == i for i in range(n))
