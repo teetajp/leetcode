7a07c80e5a52eaa1ae9f56c71d0ee01d8f179622
TJ Pavaritpong <teetajp@gmail.com>
2024-06-24 11:56:19 -0500

    Time: 51 ms (79.74%), Space: 16.7 MB (32.43%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -17,16 +17,23 @@ class Solution:
         
         Order:
         - fill linearly from MC[n] down to MC[0]
+        
+        # Time: O(n)
+        # Space: O(n)
+        
+        Since MC[i] depends only on MC[i+1] and MC[i+2],
+        we only need to store MC[i+1] and MC[i+2] instead of MC[0, 1, ..., n],
+        and in each iteration, we just need to swap move MC[i+1] to MC[i+2]
+        and overwrite MC[i+1] with MC[i].
+        
+        => Space: O(1)
         """
         n = len(cost)
-        MC = [0] * n # [0, 1, ..., n-2, n-1]
-        MC[n-1], MC[n-2] = cost[n-1], cost[n-2]
+        MC = [cost[n-2], cost[n-1]] # initialize MC[n-2] and MC[n-1]
         
         for i in reversed(range(n-2)): # n-3 down to 0
-            MC[i] = cost[i] + min(MC[i+1], MC[i+2])
+            # calculate new MC[i] and set the prev MC[i+1] to MC[i+2]
+            MC_i = cost[i] + min(MC[0], MC[1])
+            MC[0], MC[1] = MC_i, MC[0]
         
         return min(MC[0], MC[1])
-    
-    
-    # Time: O(n)
-    # Space: O(n)
