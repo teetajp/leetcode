844b51426cac3049b6da31710440a932d3f43701
TJ Pavaritpong <teetajp@gmail.com>
2024-06-24 12:00:29 -0500

    Time: 61 ms (20.97%), Space: 16.6 MB (65.56%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -31,7 +31,7 @@ class Solution:
         n = len(cost)
         MC_i1, MC_i2 = cost[n-2], cost[n-1] # initialize MC[n-2] and MC[n-1]
         
-        for i in reversed(range(n-2)): # n-3 down to 0
+        for i in range(n-3, -1, -1): # n-3 down to 0
             # calculate new MC[i] and set the prev MC[i+1] to MC[i+2]
             MC_i1, MC_i2 = cost[i] + min(MC_i1, MC_i2), MC_i1
         
