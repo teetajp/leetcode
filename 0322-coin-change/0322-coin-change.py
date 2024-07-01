9e6ad57c4dd8920031a5b66ddb7f356faa463d2b
TJ Pavaritpong <teetajp@gmail.com>
2024-06-30 20:01:19 -0500

    Time: 754 ms (74.82%), Space: 16.9 MB (77.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,36 +1,42 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Dynamic Programming problem
+        """
+        Let CC[i] be the fewest number of coins needed to make up `i` amount of money, given the coin denominations. If CC[i] is -1, then that amount cannot be made up by any combination of the coins.
         
-    # Let c[i] be the minimum number of coins needed to make i amount of money.
-    
-    # c[i] = min( coins[i - c_j] + 1, for all j < len(coins))
-        if amount == 0:
-            return 0 # base case 
+        We want to find CC[amount].
         
-        coins.sort() # smallest to largest
-        c = [math.inf for i in range(0, amount+1)]
-        # base cases
-        c[0] = 0
-        max_denom_idx = -1
-        for i in range(len(coins)):
-            denom = coins[i]
-            if denom <= amount:
-                c[denom] = 1
-                max_denom_idx = i
-            else:
-                break
-        if max_denom_idx == -1:
-            return -1
+        Constraint: 0 <= i <= amount
         
-        for a in range(1, amount+1):
-            # bottom up approach
-            for j in range(max_denom_idx, -1, -1):
-                if coins[j] <= a and a - coins[j] >= 0:
-                    c[a] = min(c[a], c[a - coins[j]] + 1)
-                
+        Base Case:
+            We can make 0 amount of money with 0 coins, so CC[0] := 0
+            For each given denomination i, there exists a coin of that amount, so CC[i] := 1
+        
+        Recursive Case:
+            CC[i] = two pointers?
             
-        if c[amount] == math.inf:
-            # no viable solution
-            return -1 
-        return c[amount] # final sol
+            
+        ------
+        Start from largest to smallest, and cache results. ==> this ensures we use the fewest number of coins
+        Skip if CC already exists.
+        """
+        coins.sort() # sort coin denominations in ascending order
+        CC = [float("inf")] * (amount + 1) # initialize DP array
+        
+        # Base cases
+        CC[0] = 0
+        
+        while coins and coins[-1] > amount:
+            del coins[-1]
+        
+        # Generate all combinations possible, caching intermediate results
+        for subamt in range(1, amount+1):
+            
+            for c in coins:
+                diff = subamt - c
+
+                if diff >= 0:
+                    CC[subamt] = min(CC[subamt], 1 + CC[diff])
+                else:
+                    continue
+
+        return CC[amount] if CC[amount] < float("inf") else -1
