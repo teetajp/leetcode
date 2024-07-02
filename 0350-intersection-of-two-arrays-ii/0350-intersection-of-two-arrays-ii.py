d1b315741bc4c2defdaf022a82b16be39e899d3c
TJ Pavaritpong <teetajp@gmail.com>
2024-07-02 11:44:26 -0500

    Time: 36 ms (98.75%), Space: 16.8 MB (31.35%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -3,6 +3,7 @@ class Solution:
         nums1, nums2 = Counter(nums1), Counter(nums2)
         res = []
         for k in set(nums1.keys()).intersection(set(nums2.keys())):
-            res.extend([k] * min(nums1[k], nums2[k]))
+            for _ in range(min(nums1[k], nums2[k])):
+                res.append(k)
         
         return res
