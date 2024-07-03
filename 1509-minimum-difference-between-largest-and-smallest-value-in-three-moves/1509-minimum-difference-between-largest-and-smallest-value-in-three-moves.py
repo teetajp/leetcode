f8c44095a16d0035edf0ce084ec3bed3dc92262a
TJ Pavaritpong <teetajp@gmail.com>
2024-07-03 15:04:22 -0500

    Time: 245 ms (96.83%), Space: 27.5 MB (39.91%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -4,24 +4,23 @@ class Solution:
     def minDifference(self, nums: List[int]) -> int:
         
         if len(nums) <= 4:
-            # can change up to 3 vals, and n == 4, then the only elem is both min and max
+            # For n <= 3, set all elems to same val
+            # For n == 4, then the only elem is both the min and the max
             return 0
         
         """
         Since we only need to get largest and smallest 4 values (3 for change, 1 to guarantee a value to compare)
         
-        We can sort in O(n + n log 4) = O(n)
+        We can sort in O(n + n log 4) = O(n) time
         
         Then we can bruteforce all (2^3 = 8) combinations of the values to pop
+        
+        Time: O(n)
+        Space: O(1)
         """
         
         minElems = heapq.nsmallest(4, nums)
         maxElems = heapq.nlargest(4, nums)
         
         # bruteforce all 2^3 choices
-#         res = maxElems[0] - minElems[0]
-        
-#         for i in range(0, 4):
-#             res = min(res, maxElems[-1-i] - minElems[i])
-            
         return min(maxElems[-1-i] - minElems[i] for i in range(0, 4))
