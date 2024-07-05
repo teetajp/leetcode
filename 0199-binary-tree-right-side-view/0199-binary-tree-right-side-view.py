160f3c2d6c686fc20532ea67a0c2bdefc80f25b1
TJ Pavaritpong <teetajp@gmail.com>
2024-07-04 21:16:44 -0500

    Time: 31 ms (88.31%), Space: 16.4 MB (69.08%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -4,23 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
-    # print current, then right, then left
-    # BFS
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        # need to go through all level and pick the right-most node (could be "left" brach of parent)
-        levels = []
-        # BFS
-        queue = deque([(root, 0)])
-        while queue:
-            node, level_idx = queue.pop()
-            if node is None:
-                continue
-            if len(levels) == level_idx:
-                # first node we see from the right side (if we go to node on the right first)
-                levels.append(node.val)
-            queue.appendleft((node.right, level_idx + 1)) # important to put this first
-            queue.appendleft((node.left, level_idx + 1))
-
-        return levels
+        """
+        Want to traverse all branches of the tree and append the rightmost node at each depth
+        """
+        res = []
+        def rightSideViewRec(cur: Optional[TreeNode], depth: int) -> None:
+            if not cur:
+                return
+            elif len(res) == depth:
+                # first time reaching this depth, so we must be in the rightmost node at this depth
+                res.append(cur.val)
+            
+            # traverse right child first so we add the rightmost node first
+            rightSideViewRec(cur.right, depth + 1)
+            rightSideViewRec(cur.left, depth + 1)
+            
+        rightSideViewRec(root, 0)
+        
+        return res
