0fb647c5b481f609a6dbfc6c58c5df5afa92713c
TJ Pavaritpong <teetajp@gmail.com>
2024-07-04 20:12:57 -0500

    Time: 5179 ms (54.28%), Space: 18 MB (77.47%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -16,7 +16,26 @@ class Trie():
             cur = cur.children[c]
         
         cur.isWord = True
-                    
+        
+    def findWords(self, res: List[str], prefix: List[str], cur: Node, row: int, col: int, board: List[List[str]]) -> None:
+        if row < 0 or row >= len(board) or col < 0 or col >= len(board[row]) or not cur or len(prefix) >= 10 or board[row][col] not in cur.children:
+            return
+
+        ltr = board[row][col]
+        prefix.append(ltr)
+        board[row][col] = '.' # mark as visited
+        
+        cur = cur.children[ltr]
+        if cur.isWord: 
+            res.append("".join(prefix)) # found new word
+            cur.isWord = False # to prevent adding the same word again
+
+        for dr, dc in ((-1, 0), (0, -1), (1, 0), (0, 1)):
+            self.findWords(res, prefix, cur, row + dr, col + dc, board)
+        
+        prefix.pop() # backtrack
+        board[row][col] = ltr # unmark
+            
         
         
 class Solution:
@@ -29,27 +48,8 @@ class Solution:
         
         res = []
         
-        def findWordsRec(prefix: List[str], cur: Node, row: int, col: int) -> None:
-            if row < 0 or row >= len(board) or col < 0 or col >= len(board[row]) or not cur or len(prefix) >= 10 or board[row][col] not in cur.children:
-                return
-
-            ltr = board[row][col]
-            prefix.append(ltr)
-            board[row][col] = '.' # mark as visited
-
-            cur = cur.children[ltr]
-            if cur.isWord: 
-                res.append("".join(prefix)) # found new word
-                cur.isWord = False # to prevent adding the same word again
-
-            for dr, dc in ((-1, 0), (0, -1), (1, 0), (0, 1)):
-                findWordsRec(prefix, cur, row + dr, col + dc)
-
-            prefix.pop() # backtrack
-            board[row][col] = ltr # unmark
-        
         for ROW in range(len(board)):
             for COL in range(len(board[ROW])):
-                findWordsRec([], trie.root, ROW, COL)
+                trie.findWords(res, [], trie.root, ROW, COL, board)
         
         return res
