cf8f945c0242f6f9df59c2dbc29d0b9e6f4e56d5
TJ Pavaritpong <teetajp@gmail.com>
2024-07-04 22:26:56 -0500

    Time: 143 ms (21.61%), Space: 17.7 MB (98.38%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        """
+        Pre-order traversal: cur, left, right
+        In-order traversal: left, cur, right
+        
+        Properties:
+        - First node in preorder traversal is always root
+        - Rightmost val is always the same for pre-order and in-order traversal.
+        - While we may not be able to tell if the next node in the preorder traversal is the left or right child (since cur node may not have both child), given that all node values are unique, we can use the in-order traversal to determine whether the next val is the preorder traversal's left child if the next val in the in-order traversal also matches.
+        - Need to locate the current node val in the inorder traversal, anything on left is on left subtree, anything on right is on right subtree.
+        """
+        p_idx = 0
+        
+        def buildTreeRec(subtree_start, subtree_end):
+            nonlocal p_idx
+            if p_idx >= len(preorder) or subtree_start >= subtree_end:
+                return None
+            
+            root_val = preorder[p_idx]
+            root_idx = None
+            for i in range(subtree_start, subtree_end):
+                if inorder[i] == root_val:
+                    root_idx = i
+                    break
+            else:
+                return None
+            
+            subtree_root = TreeNode(root_val)
+            p_idx += 1
+            
+            # if subtree_start == subtree_end - 1:
+                # return subtree_root
+            # else:
+                # subtree has at least one child
+            # root_idx = inorder[subtree_start:subtree_end+1].index(root_val)
+
+            # partition the inorder list and recurse
+            
+            subtree_root.left = buildTreeRec(subtree_start, root_idx)
+            subtree_root.right = buildTreeRec(root_idx + 1, subtree_end)
+            return subtree_root
+        
+        return buildTreeRec(0, len(inorder))
