4ee64270d33b8e83f69946483ca38db66f1bb203
TJ Pavaritpong <teetajp@gmail.com>
2024-07-06 14:28:09 -0500

    Time: 420 ms (63.70%), Space: 17.3 MB (8.22%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,39 @@
+import heapq
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        """
+        Should always complete the task that has the most freq left.
+        """
+        remainingTasks = [(-t_cnt, t_id) for t_id, t_cnt in Counter(tasks).items()] # max heap
+        coolingTasks = collections.deque() # (task, finishTime, remaining)
+        cyclesElapsed = 0 # cycles same as time
+        
+        heapq.heapify(remainingTasks)
+        
+        while remainingTasks or coolingTasks:
+            # print(remainingTasks, coolingTasks, cyclesElapsed)            
+            if coolingTasks:
+                cooledTaskId, coolEndTime, sameTaskRemaining = coolingTasks.popleft()
+                
+                if not remainingTasks:
+                    # idle this cycle; can fast forward to when next task is complete
+                    cyclesElapsed = coolEndTime
+                    heapq.heappush(remainingTasks, (-sameTaskRemaining, cooledTaskId) )
+                elif coolEndTime <= cyclesElapsed:
+                    heapq.heappush(remainingTasks, (-sameTaskRemaining, cooledTaskId) )
+                else:
+                    coolingTasks.appendleft((cooledTaskId, coolEndTime, sameTaskRemaining))
+            
+            
+            t_cnt_neg, t_id = heapq.heappop(remainingTasks) # complete a task
+            cyclesElapsed += 1
+            
+            if -t_cnt_neg > 1:
+                # need to complete more of the same task -> track its cooldown so we can repeat
+                coolingTasks.append( (t_id, cyclesElapsed + n, -t_cnt_neg - 1) )
+                
+            
+        
+        
+        return cyclesElapsed
