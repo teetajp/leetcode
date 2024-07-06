b6ef29012721966c7ef0e62d2396fa4a9776723f
TJ Pavaritpong <teetajp@gmail.com>
2024-07-06 13:12:59 -0500

    Time: 408 ms (32.44%), Space: 18.1 MB (63.60%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,33 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        """
+        BFS to find shortest length path
+        """
+        wordList = set(wordList) # use it as a list of unexplored words
+        queue = collections.deque([beginWord])
+        iterCount = 0
+        ASCII_a = ord('a')
+        
+        while queue:
+            elemsInLevel = len(queue)
+            iterCount += 1
+            
+            while elemsInLevel > 0:
+                word = queue.popleft()
+                elemsInLevel -= 1
+
+                if word == endWord:
+                    return iterCount
+                # elif word in wordList:
+                    # continue
+
+                # try changing each character in the word
+                for i in range(len(word)):
+                    for ASCII_offset in range(0, 26):
+                        nextWord = word[:i] + chr(ASCII_a + ASCII_offset) + word[i+1:]
+                        
+                        if nextWord in wordList:
+                            wordList.remove(nextWord)
+                            queue.append(nextWord)
+            
+        return 0
