5a886fdfa432f640f4716f77219364287a4202b4
TJ Pavaritpong <teetajp@gmail.com>
2024-07-06 13:22:36 -0500

    Time: 421 ms (30.85%), Space: 18.1 MB (63.60%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,6 +2,20 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         """
         BFS to find shortest length path
+        
+        # Let n be number of words in wordList.
+        # Let m be the length of each word (all are the same).
+        
+        Time Complexity:
+            Convert wordList to a set -> O(n)
+            We visit a maximum of n words since we avoid previously visited words. -> O(n)
+            
+            For each word, we try to transition to another word by changing each position to each of the 26 letters of the alphabet and checking if it forms a word in the wordList.
+            -> O(n * m * 26) = O(nm)
+            
+            Even though we could try to transition to each word in the wordList from current word, because we remove visited words from the `wordList`, in total, we only visit O(n) words.
+            
+            So total time complexity is O(nm), and space complexity is O(n)
         """
         wordList = set(wordList) # use it as a list of unexplored words
         queue = collections.deque([beginWord])
