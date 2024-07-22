77fb92e9761f0539f968891562a2e864e9d5b486
TJ Pavaritpong <teetajp@gmail.com>
2024-07-21 22:30:49 -0500

    Time: 58 ms (46.78%), Space: 17 MB (57.91%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -9,16 +9,6 @@ class Solution:
                 return frozenset()
             
             removed = set()
-            # offset_diag, offset_antidiag = col - 1, col + 1
-            # for r in range(row - 1, -1, -1):
-            #     # remove diagonals (top left to bottom right)
-            #     if offset_diag >= 0:
-            #         removed.add( (r, offset_diag) )
-            #         offset_diag -= 1
-            #     # remove anti-diagonals (top right to bottom left)
-            #     if offset_antidiag < n:
-            #         removed.add( (r, offset_antidiag) )
-            #         offset_antidiag += 1
             
             offset_diag, offset_antidiag = col, col
             for r in range(row, n):
@@ -35,15 +25,11 @@ class Solution:
         
         res = []
         colsOpen = [True for _ in range(n)]
-        assignments = []
-        
-        def copyBoard():
-            newBoard = ['.' * c + 'Q' + '.' * (n - c - 1) for c in assignments]
-            res.append(newBoard)
+        board = [['.' for c in range(n)] for r in range(n)]
         
         def solveRec(row, diagsBlocked):
             if row == n:
-                copyBoard()
+                res.append(["".join(cols) for cols in board])
                 return
             
             for col in range(n):
@@ -52,13 +38,12 @@ class Solution:
                     # update params for new iter
                     newDiagsBlocked = diagsBlocked.union(removeDiagonals(row, col)) # immutable set
                     colsOpen[col] = False
-                    assignments.append(col)
+                    board[row][col] = 'Q'
                     # recurse
                     solveRec(row+1, newDiagsBlocked)
                     # backtrack
-                    assignments.pop()
+                    board[row][col] = '.'
                     colsOpen[col] = True
-                    # diagsBlocked.difference_update(diagsAttackedByCur)
                     
         solveRec(0, frozenset())
         return res
