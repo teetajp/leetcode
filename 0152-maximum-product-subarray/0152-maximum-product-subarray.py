5168d1b679967389f784daaebdab2a5622d30705
TJ Pavaritpong <teetajp@gmail.com>
2024-07-25 15:09:32 -0500

    Time: 72 ms (55.72%), Space: 17.1 MB (14.65%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        """
+        Cases:
+        - 0
+        - neg
+        - pos
+        
+        
+        let DP[i] be the max subarray product between index 0 and i, including the current element
+        
+        use mins array as well?
+        
+        if nums[i] == 0: dp[i] = max(dp_max[i-1], 0)
+        if nums[i] > 0: dp[i] = max(dp_max[i-1] * nums[i], nums[i])
+        if nums[i] < 0: dp[i] = max(dp_max[i-1], dp_min[i-1] * nums[i], nums[i])
+        
+        if nums[i] == 0: dp[i] = min(dp_min[i-1], 0)
+        if nums[i] > 0: dp[i] = min(dp_min[i-1] * nums[i], nums[i])
+        if nums[i] < 0: dp[i] = min(dp_max[i-1] * nums[i], nums[i])
+        """
+        res = max(nums)
+        maxProd, minProd = 1, 1
+        
+        for i in nums:
+            if i == 0:
+                maxProd = minProd = 1
+                continue
+
+            maxProd, minProd = max(minProd * i, i, i * maxProd), min(minProd * i, i, i * maxProd)
+                
+            res = max(res, maxProd, minProd)
+            
+            
+        return res
