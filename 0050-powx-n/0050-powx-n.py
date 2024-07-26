c3472cd2bb7e33fcfc42cf03016ef25edf1a5752
TJ Pavaritpong <teetajp@gmail.com>
2024-07-26 15:51:43 -0500

    Time: 46 ms (6.35%), Space: 16.4 MB (80.80%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,20 @@
+import sys
+ 
+sys.setrecursionlimit(10**6)
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        """
+        -100.0 < x < 100.0
+        but -2^31 <= n <= 2^31 - 1
+        so the exponent can be very big and dominates the running time
+        """
+        if n == 0:
+            return 1
+        elif n < 0:
+            return 1 / self.myPow(x, -n)
+        
+        if n % 2 == 1:
+            # odd
+            return x * self.myPow(x * x, n // 2)
+        else:
+            return self.myPow(x * x, n // 2)
