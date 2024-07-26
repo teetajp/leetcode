87bddeb532267363e27f56c53417b71f97991aaf
TJ Pavaritpong <teetajp@gmail.com>
2024-07-25 20:54:34 -0500

    Time: 29 ms (90.55%), Space: 16.6 MB (29.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -6,12 +6,23 @@ class Solution:
         dp[0][j] = 1
         dp[i][0] = 1
         dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        to further optimize, get the min of (m, n)
         """
-        
-        dp = [1] * n
-        
-        for _ in range(1, m):
-            for j in range(1, n):
-                dp[j] = dp[j-1] + dp[j] # left + up
+        if n < m:
+            dp = [1] * n
+
+            for _ in range(1, m):
+                for j in range(1, n):
+                    dp[j] = dp[j-1] + dp[j] # left + up
+                    
+            return dp[n-1]
+        else:
+            dp = [1] * m
+            
+            for _ in range(1, n):
+                for i in range(1, m):
+                    dp[i] = dp[i-1] + dp[i] # left + up
+            
                 
-        return dp[n-1]
+            return dp[m-1]
