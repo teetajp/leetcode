c7b0cd6688d6f060fc489d8f4ae63305846f124e
TJ Pavaritpong <teetajp@gmail.com>
2024-07-25 20:08:11 -0500

    Time: 26 ms (98.85%), Space: 16.5 MB (36.11%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -31,13 +31,12 @@ class Solution:
         for i in range(n-1):
             for j in range(i, n-1-i):
 
-                start = (i, j)
                 cur_r, cur_c = None, None
                 cur_val = matrix[i][j]
                 nxt_r, nxt_c = j, n - i - 1
 
 
-                while (cur_r, cur_c) != start:
+                while cur_r != i or cur_c != j:
                     matrix[nxt_r][nxt_c], cur_val = cur_val, matrix[nxt_r][nxt_c]
                     cur_r, cur_c = nxt_r, nxt_c
                     nxt_r, nxt_c = cur_c, n - cur_r - 1
