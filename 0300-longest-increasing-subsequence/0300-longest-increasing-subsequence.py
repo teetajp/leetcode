9422aa2fefd93371f0578d9646da2bfcfafa0bec
TJ Pavaritpong <teetajp@gmail.com>
2024-07-28 23:52:00 -0500

    Time: 109 ms (78.63%), Space: 17.6 MB (10.41%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,85 +1,12 @@
 from sortedcontainers import SortedList
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        """
-        Strictly increasing subsequence:
-        - Define a subsequence of `nums`:
-            ss := [nums_i1, nums_i2, ..., nums_ik] where nums[i_j] < nums[i_j+1] < ... < nums[i_k] for 0 <= i_j < ... < i_k < len(nums)
-        - Given that len(nums) > 0, there always exists a subsequence of length 1 or more (i.e. a subseq of any elem of nums)
-        
-        Brute-force:
-        - generate all 2^n subsequences of `nums`, then check whether they satisfy the strictly increasing property, and get any subseq that has the maximum length (multiple subsequences may all tie for max length)
-        - time: O(2^n)
-        - space: O(n)
-        
-        Dynamic Programming:
-        - Candidates:
-            - Let maxLIS[i] be the length of the longest strictly increasing subsequence in `nums` from idx 0 to idx i, where nums[i] is included in the subsequence.
-                - Need to keep some kind of monotonic stack / queue ? heap?
-                - Need to lookback to maxLIS 0 to i-1?
-                    - could be O(n^2) runtime, still better than O(2^n) ... could work
-                    - look for first value of maxLIS[j] that is than nums[i], use that as baseline?
-                        - best would be slightly smaller than nums[i], that would get more length than j that is close to i but has nums[j] a lot less than nums[i] ...
-                            - do we still need to look for a smaller one after we find the first one?
-                            - could cut search space shorter if we find one a maxLIS[j] close to but less than nums[i], but still need to lookback a bit more, just until length of j is roughly equal to maxLIS[j], since there cant be a subseq longer than length j... but overall still need to check... and doesn't improve runtime by a polynomial factor...
-            - Let maxLIS[i] be the length of the longest strictly increasing subsequence in `nums` from idx 0 to idx i.
-                - How to keep track of what was the last elem value?
-                - maxLIS[i] = max(maxLIS[i-1], max(maxLIS[j] where nums[j] < nums[i]))
-                    - compare best maxLIS without including current index nums[i], and the max length subseq including nums[i]
-                        - use binary search tree to find next lower index?
-                        - binary search to search / insert each elem ---> red-black / AVL tree ---> O( log(n) ) per operation
-                            - O(1) to get maxLIS[i-1], O(log n) to find maxLIS[j] where nums[j] < nums[i]
-                            - O(n log(n) ) total!!!
-                                - use Python's bisect module, or the `sortedcontainers` module
-                    - how to find the next lower elem?
-                    - reduce from 2D maxLIS[0 <= i <= n-1][True/False] to maxLIS[i] ?
-            - monotonic stack/queue approach with no DP?
-            
-            
-        Let's implement the O(n^2) algorithm first, then implement the O(n log n) algorithm later
-        
-        ```
-        Assume n = len(nums) > 0.
-        Let maxLIS[i] be the length of the longest strictly increasing subsequence in `nums` from idx 0 to idx i, where nums[i] is included in the sequence.
-        
-        Base Case:
-            maxLIS[0] = 1
-        Recursive Case:
-            For 0 < i < n,
-                maxLIS[i] = max( 1 + max(maxLIS[j] where nums[j] < nums[i] and 0 <= j < i) )
-            
-        Order of Evaluation:
-            Evaluate `maxLIS[i]`
-                for i := 0 -> n-1,
-                for j < i,
-                    j := i-1 -> 0
-        
-        Final Answer: maxLIS[n-1]
-        
-        Time Complexity: O(n^2); We take O(n) to iterate over each element of nums, and for each element, we do a linear scan backwards for j := i-1 -> 0, which takes O(n), so the algorithm takes O(n^2) in total.
-        Space Complexity: O(n) for `maxLIS` array
-        ```
-        """
-        res: int = 1
-        n: int = len(nums)
-        maxLIS: SortedList[int] = SortedList([(nums[0], 1)])
-        # maxLIS: list[int] = [1] * n # initialize DP array; lower bound of all answer is 1 (subseq with just the element)
-        
-        for val in nums[1:]: 
-            # maxLIS[i]: int = 1 + max((maxLIS[j] for j in reversed(range(0, i)) if nums[j] < nums[i]), default=0)
-            prevMaxLIS = 0
-            foundPrevMax = False
-            pivot = maxLIS.bisect_left( (val, n) ) # end idx where all elems < nums[i]
-            for i in reversed(range(0, min(len(maxLIS),pivot+1))):
-                # if maxLIS[i] < val
-                if maxLIS[i][0] < val and maxLIS[i][1] > prevMaxLIS:
-                    prevMaxLIS = maxLIS[i][1]
-                    foundPrevMax = True
-            
-            curMaxLIS = prevMaxLIS + 1 if foundPrevMax else 1
-            maxLIS.add( (val, curMaxLIS) )
-            res: int = curMaxLIS if curMaxLIS > res else res
-        
-        
-        return res
-        
+        max_lens = SortedList()
+        for i in range(len(nums)):
+            p = max_lens.bisect_right((nums[i], 0))
+            l = 1 if p == 0 else max_lens[p-1][1] + 1
+            while p < len(max_lens) and max_lens[p][1] <= l:
+                max_lens.pop(p)
+            max_lens.add((nums[i], l))
+        return max_lens[-1][1]
