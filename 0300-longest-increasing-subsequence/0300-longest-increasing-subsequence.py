677037fd4248dc92ae4f4a65e71a2786e91fe2cb
TJ Pavaritpong <teetajp@gmail.com>
2024-07-28 23:07:05 -0500

    Time: 1208 ms (71.24%), Space: 16.9 MB (29.26%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -59,16 +59,13 @@ class Solution:
         Space Complexity: O(n) for `maxLIS` array
         ```
         """
+        res: int = 1
         n: int = len(nums)
         maxLIS: list[int] = [1] * n # initialize DP array; lower bound of all answer is 1 (subseq with just the element)
-        res = 1
-        for i in range(1, n):
-            
-            # length of maxLIS before iteration i, where sequence was lower in value
-            maxLIS[i] = 1 + max((maxLIS[j] for j in reversed(range(0, i)) if nums[j] < nums[i]), default=0)
-            
-            
-            res = maxLIS[i] if maxLIS[i] > res else res
+        
+        for i in range(1, n):    
+            maxLIS[i]: int = 1 + max((maxLIS[j] for j in reversed(range(0, i)) if nums[j] < nums[i]), default=0)
+            res: int = maxLIS[i] if maxLIS[i] > res else res
         
         
         return res
