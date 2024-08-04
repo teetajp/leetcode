be6ceee046c52ca3fcc2015ae0834fb55b6da352
TJ Pavaritpong <teetajp@gmail.com>
2024-08-04 18:51:35 -0500

    Time: 12 ms (91.88%), Space: 2.7 MB (92.19%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.go
@@ -0,0 +1,31 @@
+func canPartition(nums []int) bool {
+    sum := 0
+    
+    for i := range nums {
+        sum += nums[i]
+    }
+    
+    target := sum / 2
+
+    if (sum % 2 != 0) || (len(nums) == 1 && nums[0] != 0) {
+        return false
+    } else if target == 0 {
+        return true
+    }
+    
+    
+    dp := make([]bool, target + 1)
+    dp[0] = true
+    
+    for _, val := range nums {
+        for i := target; i >= val; i-- {
+            dp[i] = dp[i] || dp[i - val]
+        }
+        
+        if dp[target] {
+            return true
+        }
+    }
+    
+    return dp[target]
+}
